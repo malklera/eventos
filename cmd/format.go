eb021dec26ecfa07b91078a3915e0dd3502fbeba
malklera <malklerulo@gmail.com>
2026-06-13 17:06:19 -0300

    feat: add format cmd

---
+++ cmd/format.go
@@ -0,0 +1,79 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// formatCmd represents the format command
+var formatCmd = &cobra.Command{
+	Use:   "format",
+	Short: "Makes the videos fixed frame rate and took out sound.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		src := "renombrado"
+		dst := "formateado"
+		files, err := os.ReadDir(src)
+		if err != nil {
+			return fmt.Errorf("error reading %s: %w", src, err)
+		}
+		count := len(files)
+		done := 0
+		start := time.Now()
+		for _, file := range files {
+			srcF := filepath.Join(src, file.Name())
+			dstF := filepath.Join(dst, file.Name())
+			fmt.Println("Formateando:", srcF)
+			ffmpeg := exec.Command("ffmpeg",
+				"-v",
+				"warning",
+				"-i",
+				srcF,
+				"-vf",
+				"fps=30",
+				"-c:v",
+				"libx264",
+				"-preset",
+				"medium",
+				"-crf",
+				"23",
+				"-movflags",
+				"+faststart",
+				"-an",
+				dstF)
+			ffmpeg.Stdout = os.Stdout
+			ffmpeg.Stderr = os.Stderr
+			err := ffmpeg.Run()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error running %s: %v\n", ffmpeg.String(), err)
+			} else {
+				done++
+			}
+		}
+		fmt.Println("Total files:", count)
+		fmt.Println("Files formated:", done)
+		fmt.Println("Time taken:", time.Since(start))
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(formatCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// formatCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// formatCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
